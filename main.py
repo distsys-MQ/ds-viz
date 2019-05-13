376a7095e85721ef207aec002d3edaa61c5fca2c
JaydenKing32 <jayden.king@students.mq.edu.au>
2019-05-13 23:38:01 +1000

    Refactored Server variables

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-from asciimatics.screen import Screen
-
 from server import get_servers
 
 servers = get_servers()
+WIDTH = 80
 
-
-def demo(screen: Screen):
-    while True:
-        screen.refresh()
-        pos = 0
-        for typ, dic in servers.items():
-            for i, s in dic.items():
-                screen.print_at("{} {}".format(typ, i), 0, pos)
-                pos += s.cores * 3
-
-        if screen.get_key() is Screen.KEY_DOWN:
-            screen.scroll()
-        elif screen.get_key() is Screen.KEY_UP:
-            screen.scroll_to(2)
-
-
-Screen.wrapper(demo, height=2000)
+for s in servers:
+    print("{} {}".format(s.kind, s.sid))
+    print()
+    print("|\n" * s.cores)
+    print("=" * WIDTH)
--- server.py
+++ server.py
@@ -1,24 +1,19 @@
-from typing import Dict
+from typing import List
 from xml.etree.ElementTree import parse
 
 
 class Server:
-    def __init__(self, server):
-        self.boot = int(server["bootupTime"])
-        self.cores = int(server["coreCount"])
-        self.memory = int(server["memory"])
-        self.disk = int(server["disk"])
+    def __init__(self, kind: str, sid: int, cores: int):
+        self.kind = kind
+        self.sid = sid
+        self.cores = cores
 
 
-def get_servers() -> Dict[str, Dict[int, Server]]:
-    res: Dict[str, Dict[int, Server]] = {}
+def get_servers() -> List[Server]:
+    servers = []
 
-    for serve in parse("system.xml").iter("server"):
-        t = serve.attrib["type"]
-        res[t] = {}
+    for s in parse("system.xml").iter("server"):
+        for i in range(int(s.attrib["limit"])):
+            servers.append(Server(s.attrib["type"], i, int(s.attrib["coreCount"])))
 
-        for i in range(int(serve.attrib["limit"])):
-            s = {a: serve.attrib[a] for a in serve.attrib if a not in ("type", "limit")}
-            res[t][i] = Server(s)
-
-    return res
+    return servers
