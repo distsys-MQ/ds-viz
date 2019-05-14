c9f6001017d457e11e852b64342698a5ecb03ae5
JaydenKing32 <jayden.king@students.mq.edu.au>
2019-05-15 01:05:08 +1000

    Added references for drawing boxes

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ for s in servers:
     print()
     print("|\n" * s.cores)
     print("=" * WIDTH)
+
+
+# https://stackoverflow.com/q/20756516/8031185
+# https://stackoverflow.com/a/47614884/8031185
+# https://stackoverflow.com/q/35381065/8031185
