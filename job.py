c46dea4003f18b747db724a0cc1397960e35274f
JaydenKing32 <jayden.king@students.mq.edu.au>
2019-07-21 15:29:42 +1000

    Reset to before buggy job failures, need to re-implement job failures

--- job.py
+++ job.py
@@ -1,22 +1,13 @@
 from typing import Dict, List, BinaryIO
 
-from job_failure import JobFailure
-
 
 class Job:
-    def __init__(self, jid: int, cores: int, schd: int = None, start: int = None, end: int = None,
-                 failures: List[JobFailure] = None):
+    def __init__(self, jid: int, cores: int, schd: int = None, start: int = None, end: int = None):
         self.jid = jid
         self.cores = cores
         self.schd = schd
         self.start = start
         self.end = end
-        self.fail_count = 0
-
-        if failures is None:
-            self.failures = []
-        else:
-            self.failures = failures
 
 
 def get_jobs(file: str, servers) -> List[Job]:
@@ -29,10 +20,6 @@ def get_jobs(file: str, servers) -> List[Job]:
             if b"JOBN" in line:
                 f.seek(-len(line), 1)
                 jobs.append(make_job(f, servers))
-
-            if b"JOBF" in line:
-                f.seek(-len(line), 1)
-                add_failure(f, jobs)
 
             if not line:
                 break
@@ -71,49 +58,12 @@ def get_job_times(file: str, jobs: Dict[int, Job]):
                 msg = line.split()
                 jid = int(msg[3])
                 time = int(msg[1])
-                job = jobs[jid]
-
-                if jid == 1831:
-                    print("test")
 
                 # TODO try replacing with a dictionary
                 #  https://docs.quantifiedcode.com/python-anti-patterns/readability/not_using_if_to_switch.html
-                if "SCHEDULED" in msg and job.schd is None:
-                    job.schd = time
+                if "SCHEDULED" in msg:
+                    jobs[jid].schd = time
                 elif "RUNNING" in msg:
-                    if job.start is None:
-                        job.start = time
-
-                        jf = next((f for f in job.failures if f.start == -1), None)
-                        if jf:
-                            jf.start = time
-                    else:
-                        job.failures[job.fail_count].start = time
-                        job.fail_count += 1
-                elif "COMPLETED" in msg and job.end is None:
-                    job.end = time
-
-
-def add_failure(f: BinaryIO, jobs: List[Job]):
-    msg = f.readline().decode("utf-8").split()
-    jid = int(msg[3])
-    time = int(msg[2])
-    job = next((j for j in jobs if j.jid == jid))
-
-    if job.start is None:
-        start = -1
-    else:
-        start = None
-
-    while True:
-        line = f.readline()
-
-        if b"SCHD" in line:
-            msg = line.decode("utf-8").split()
-
-            jf = JobFailure(time, start, msg[3], int(msg[4]))
-            job.failures.append(jf)
-            return
-
-        if not line:
-            break
+                    jobs[jid].start = time
+                elif "COMPLETED" in msg:
+                    jobs[jid].end = time
--- sim_viz.py
+++ sim_viz.py
@@ -1,11 +1,10 @@
-from typing import List, Dict
+from typing import List
 
 import numpy as np
 import PySimpleGUI as pSG
 
 from job import Job
-from job_failure import JobFailure
-from server import Server, get_servers_from_system
+from server import get_servers_from_system
 from server_failure import ServerFailure
 
 servers = get_servers_from_system("bf-100.txt", "fail-free-config100.xml")
@@ -35,22 +34,8 @@ def norm_jobs(jobs: List[Job]) -> List[Job]:
     arr = np.array([(j.start, j.end) for j in jobs])
     arr = np.interp(arr, (0, end_time), (x_offset, width - margin))
 
-    return [Job(j.jid, j.cores, j.schd, start, end, j.failures)
+    return [Job(j.jid, j.cores, j.schd, start, end)
             for (start, end), j in zip([(int(i), int(k)) for (i, k) in arr], jobs)]
-
-
-def norm_job_failures(failures: List[JobFailure]) -> List[JobFailure]:
-    if not failures:
-        return []
-
-    for jf in failures:
-        if jf.start is None:
-            print(f"{jf.s_kind} {jf.sid} {jf.reschd}")
-
-    arr = np.array([jf.start for jf in failures])
-    arr = np.interp(arr, (0, end_time), (x_offset, width - margin))
-
-    return [JobFailure(jf.reschd, start, jf.s_kind, jf.sid) for start, jf in zip([int(r) for r in arr], failures)]
 
 
 def norm_server_failures(failures: List[ServerFailure]) -> List[ServerFailure]:
@@ -63,59 +48,28 @@ def norm_server_failures(failures: List[ServerFailure]) -> List[ServerFailure]:
     return [ServerFailure(fail, recover) for (fail, recover) in [(int(f), int(r)) for (f, r) in arr]]
 
 
-def init_server_dict(svrs: List[Server]) -> Dict[str, Dict[int, int]]:
-    s_dict = {}
-
-    for s in svrs:
-        if s.kind not in s_dict:
-            s_dict[s.kind] = {}
-
-        s_dict[s.kind][s.sid] = None
-
-    return s_dict
-
-
 def draw():
-    svr_ys = init_server_dict(servers)
     top = height - margin
     last = top
 
     for i, s in enumerate(servers):
         offset = s.cores * l_width + l_width
         y = last - offset
-        svr_ys[s.kind][s.sid] = y
         graph.DrawText("{} {}".format(s.kind, s.sid), (margin, y))
 
         if len(s.jobs) == 0:
             last -= s.cores * l_width + l_width
 
-        for j in norm_jobs(s.jobs):
-            if j.failures:
-                j.failures = norm_job_failures(j.failures)
+        for job in norm_jobs(s.jobs):
+            for k in range(job.cores):
+                job_y = y + k * l_width
+                last = min(last, job_y)
+                graph.DrawLine((job.start + margin, job_y), (job.end, job_y), width=l_width)
 
-                for k in range(j.cores):
-                    jy = y + k * l_width
-                    last = min(last, jy)
-                    graph.DrawLine((j.start + margin, jy), (j.failures[0].start, jy), width=l_width, color="blue")
-
-                # next_starts = [jf.start for jf in j.failures[1:]]
-                # next_starts.append(j.end)
-                #
-                # for jf, ns in zip(j.failures, next_starts):
-                #     for k in range(j.cores):
-                #         svr_y = svr_ys[jf.s_kind][jf.sid]
-                #         jy = svr_y + k * l_width
-                #         graph.DrawLine((jf.reschd + margin, jy), (ns, jy), width=l_width, color="blue")
-
-            else:
-                for k in range(j.cores):
-                    jy = y + k * l_width
-                    last = min(last, jy)
-                    graph.DrawLine((j.start + margin, jy), (j.end, jy), width=l_width, color="green")
-
-        for f in norm_server_failures(s.failures):
-            f_y = y - 2
-            graph.DrawRectangle((f.fail, f_y), (f.recover, f_y + s.cores * l_width), fill_color="red", line_color="red")
+        for fail in norm_server_failures(s.failures):
+            fail_y = y - 2
+            graph.DrawRectangle((fail.fail, fail_y), (fail.recover, fail_y + s.cores * l_width),
+                                fill_color="red", line_color="red")
 
 
 draw()
