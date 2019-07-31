9f03cb77049bd13c795c31aae434b51e9639d420
JaydenKing32 <jayden.king@students.mq.edu.au>
2019-07-31 22:23:23 +1000

    Added test file

---
+++ psg_test.py
@@ -0,0 +1,14 @@
+import PySimpleGUI as pSG
+
+width = 1000
+height = 600
+pSG.SetOptions(font=("Helvetica", 10))
+layout = [
+    [pSG.Text("=" * 162)],
+    [pSG.Slider(range=(0, 1000), default_value=0, size=(100, 15), orientation="h")]
+]
+window = pSG.Window("test", layout)
+window.Finalize()
+
+while True:
+    event, values = window.Read()
