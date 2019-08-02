fb9aaca3ffb6ccb13d85804ad856c1b4dd91ead2
JaydenKing32 <jayden.king@students.mq.edu.au>
2019-08-02 20:57:54 +1000

    Replaced font with monospaced font for consistency

--- psg_test.py
+++ psg_test.py
@@ -1,12 +1,31 @@
 import PySimpleGUI as pSG
 
-pSG.SetOptions(font=("Helvetica", 10))
+width = 100
+height = 60
+text = "testing"
+font = ("Courier New", 10)
+
+pSG.SetOptions(font=font, element_padding=(0, 0))
 layout = [
-    [pSG.Text("=" * 162)],
-    [pSG.Slider(range=(0, 1000), default_value=0, size=(100, 15), orientation="h")]
+    [pSG.Text("Testing0")],
+    [pSG.Graph((width, height), graph_bottom_left=(0, height), graph_top_right=(width, 0),
+               key="graph", pad=(0, 0))]
 ]
-window = pSG.Window("test", layout)
-window.Finalize()
+
+window = pSG.Window("test", layout, margins=(0, 0))
+graph = window.Finalize().Element("graph")
+graph.DrawText("Testing1", (0, 0), font=font)
+graph.DrawText("Testing2", (width, 0), font=font)
+graph.DrawText("Testing3", (25, 15), font=font)
+graph.DrawText("Testing4", (width/2, height/2), font=font)
+graph.DrawText("Testing5", (0, height), font=font)
+graph.DrawText("Testing6", (width, height), font=font)
+graph.DrawText("X", (7, 45), font=font)
 
 while True:
     event, values = window.Read()
+
+    if event is None or event == 'Exit':
+        break
+
+window.Close()
--- sim_viz.py
+++ sim_viz.py
@@ -36,7 +36,7 @@ right_margin = 15
 last_time = Server.last_time
 x_offset = left_margin * 2
 
-pSG.SetOptions(font=("Helvetica", 10), background_color="whitesmoke", element_padding=(0, 0))
+pSG.SetOptions(font=("Courier New", 10), background_color="whitesmoke", element_padding=(0, 0))
 
 graph_column = [[pSG.Graph(canvas_size=(width, height), graph_bottom_left=(0, 0), graph_top_right=(width, height),
                            key="graph", change_submits=True, drag_submits=False)]]
@@ -45,7 +45,7 @@ layout = [
     [pSG.Frame("Current Server", [[pSG.Txt("", size=frame_size, key="current_server")]]),
      pSG.Frame("Current Results", [[pSG.Txt("", size=frame_size, key="current_results")]]),
      pSG.Frame("Final Results", [[
-         pSG.Column([[pSG.Txt(get_results(args.log), font=("Helvetica", 7))]],
+         pSG.Column([[pSG.Txt(get_results(args.log), font=("Courier New", 8))]],
                     size=(350, 85), scrollable=True)]])],
     [pSG.Slider(range=(0, Server.last_time), default_value=0, size=(89, 15), pad=((x_offset - 7, 0), 0),
                 orientation="h", enable_events=True, key="slider")],
@@ -89,13 +89,14 @@ box_x2 = x_offset - 2
 def draw() -> None:
     top = height - left_margin
     last = top
+    font = ("Courier New", 8)
 
     for kind, kind_dict in s_dict.items():
         # Get first server of the current kind to calculate the kind's offset
         # Defining a 'kind' separately could make this easier
         kind_offset = list(kind_dict.values())[0].cores * l_width + l_width
         text_margin = 6
-        graph.DrawText(f"{kind}", (left_margin - text_margin, last - kind_offset))
+        graph.DrawText(f"{kind}", (left_margin - text_margin, last - kind_offset), font=font)
 
         for s in kind_dict.values():
             offset = s.cores * l_width + l_width
@@ -106,7 +107,7 @@ def draw() -> None:
             graph.DrawRectangle((box_x1, box_y1), (box_x2, box_y2))
 
             sid_y = last - offset
-            graph.DrawText(f"{s.sid}", (x_offset - text_margin, sid_y))
+            graph.DrawText(f"{s.sid}", (x_offset - text_margin, sid_y), font=font)
 
             if len(s.jobs) == 0:  # Add empty space for jobless servers
                 last -= s.cores * l_width + l_width
