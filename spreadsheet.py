d1b25ed57598d7a9b922f6a0dec8ec272550812a
JaydenKing32 <jayden.king@students.mq.edu.au>
2019-08-10 17:18:22 +1000

    Added get_filename_info

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,14 +1,18 @@
 import csv
 import os
 import re
-from typing import Union, Callable, Dict
+from typing import Union, Callable, Dict, List
 
 from file_read_backwards import FileReadBackwards
 from natsort import natsorted
 
 
 class Result:
-    def __init__(self, servers: int, utilisation: float, cost: float, waiting: int, execution: int, turnaround: int):
+    def __init__(self, model: str, algorithm: str, size: int, servers: int, utilisation: float,
+                 cost: float, waiting: int, execution: int, turnaround: int):
+        self.model = model
+        self.algorithm = algorithm
+        self.size = size
         self.servers = servers
         self.utilisation = utilisation
         self.cost = cost
@@ -26,6 +30,17 @@ def extract(find: str, string: str, type_: Callable) -> Union[int, float]:
     return type_(re.search(find + r"(\d+\.?\d*)", string).group(1))
 
 
+def get_filename_info(filename: str) -> Dict[str, str]:
+    res = {}
+    cats = re.split(r"[\-.]", filename.replace("config", ''))[:-2]
+
+    res["model"] = cats[0]
+    res["algo"] = cats[1]
+    res["size"] = cats[2]
+
+    return res
+
+
 def make_result(log: str) -> Result:
     raw_text = parse_log(log)
 
@@ -36,24 +51,39 @@ def make_result(log: str) -> Result:
     execute = extract(r"avg exec time: ", raw_text, int)
     turn = extract(r"avg turnaround time: ", raw_text, int)
 
-    return Result(servers, util, cost, wait, execute, turn)
+    f_info = get_filename_info(os.path.basename(log))
+    model = f_info["model"]
+    algo = f_info["algo"]
+    size = int(f_info["size"])
+
+    return Result(model, algo, size, servers, util, cost, wait, execute, turn)
 
 
-def get_results(folder: str) -> Dict[str, Dict[str, Dict[int, Result]]]:
+def get_results(folder: str) -> List[Result]:
+    results = []
+
+    for filename in natsorted(os.listdir(folder)):
+        res = make_result(f"./{folder}/{filename}")
+        results.append(res)
+
+    return results
+
+
+def get_results_dict(folder: str) -> Dict[str, Dict[str, Dict[int, Result]]]:
     result_d = {}
 
     for filename in os.listdir(folder):
-        cats = re.split(r"[\-.]", filename.replace("config", ''))[:-2]
-        f_model = cats[0]
-        algo = cats[1]
-        size = int(cats[2])
+        f_info = get_filename_info(filename)
+        model = f_info["model"]
+        algo = f_info["algo"]
+        size = int(f_info["size"])
 
-        if f_model not in result_d:
-            result_d[f_model] = {}
-        if algo not in result_d[f_model]:
-            result_d[f_model][algo] = {}
+        if model not in result_d:
+            result_d[model] = {}
+        if algo not in result_d[model]:
+            result_d[model][algo] = {}
 
-        result_d[f_model][algo][size] = make_result(f"./{folder}/{filename}")
+        result_d[model][algo][size] = make_result(f"./{folder}/{filename}")
 
     return result_d
 
@@ -75,4 +105,4 @@ def make_spreadsheet(results: Dict[str, Dict[str, Dict[int, Result]]], filename:
                                      res.cost, res.waiting, res.execution, res.turnaround])
 
 
-make_spreadsheet(get_results("test-logs"))
+make_spreadsheet(get_results_dict("test-logs"))
