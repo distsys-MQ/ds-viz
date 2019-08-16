6199b08bba4c888bddf2543780d34065d9f4e21a
JaydenKing32 <jayden.king@students.mq.edu.au>
2019-08-16 17:23:40 +1000

    Removed old and unnecessary modules

--- job_failure.py
+++
@@ -1,6 +0,0 @@
-class JobFailure:
-    def __init__(self, reschd: int, start: int = None, s_kind: str = None, sid: int = None):
-        self.reschd = reschd
-        self.start = start
-        self.s_kind = s_kind
-        self.sid = sid
--- server_config_gen.py
+++
@@ -1,3 +0,0 @@
-for i in range(100):
-    print('<server type="{}" limit="10"'
-          'bootupTime="60" hourlyRate="0.4" coreCount="4" memory="16000" disk="64000" />'.format(hex(i)[2:].zfill(2)))
