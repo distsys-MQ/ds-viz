3dafdbb0f576a1305dc12b8239fe676687aae977
JaydenKing32 <jayden.king@students.mq.edu.au>
2019-09-01 16:07:08 +1000

    Improved argument checking

--- sim_viz.py
+++ sim_viz.py
@@ -1,27 +1,20 @@
-import os
 from argparse import ArgumentParser
 
+from arg_checks import IsFile, MinInt
 from visualisation import Visualisation
 
-
-# TODO Improve argument checking (e.g. positive ints)
-#  Could sub-class argparse https://stackoverflow.com/a/18700817/8031185
-
-# https://stackoverflow.com/a/11541450/8031185
-def is_valid_file(psr: ArgumentParser, arg: str) -> str:
-    if not os.path.isfile(arg):
-        psr.error("The file '{}' does not exist!".format(arg))
-    else:
-        return arg
-
-
 parser = ArgumentParser(description="Visualises DS simulations")
-parser.add_argument("config", type=lambda f: is_valid_file(parser, f), help="configuration file used in simulation")
+parser.add_argument("config", action=IsFile,
+                    help="configuration file used in simulation")
 # TODO Work out how to make failures optional while maintaining argument order
-parser.add_argument("failures", type=lambda f: is_valid_file(parser, f), help="resource-failures file from simulation")
-parser.add_argument("log", type=lambda f: is_valid_file(parser, f), help="simulation log file to visualise")
-parser.add_argument("-c", "--core_height", type=int, default=4, help="set core height")
-parser.add_argument("-s", "--scale", type=int, default=0, help="set scaling factor of visualisation")
+parser.add_argument("failures", action=IsFile,
+                    help="resource-failures file from simulation")
+parser.add_argument("log", action=IsFile,
+                    help="simulation log file to visualise")
+parser.add_argument("-c", "--core_height", type=int, default=4, action=MinInt, min_int=1,
+                    help="set core height, minimum value of 1")
+parser.add_argument("-s", "--scale", type=int, default=0, action=MinInt,
+                    help="set scaling factor of visualisation")
 args = parser.parse_args()
 
 viz = Visualisation(args.config, args.failures, args.log, args.core_height, args.scale)
