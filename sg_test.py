e8712f00247de10f354bf792161b615630e5dc97
JaydenKing32 <jayden.king@students.mq.edu.au>
2019-09-08 20:16:04 +1000

    Layout testing

--- sg_test.py
+++ sg_test.py
@@ -1,24 +1,33 @@
 import PySimpleGUI as sg
 
-num = 4
-text = '\n'.join(
-    ['.'.join(hex(k * num + i)[2:].zfill(2) for i in range(num)) for k in range(num)]
+text = """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
+anim id est laborum."""
+
+font1 = ("Times", 12)
+font2 = ("Courier", 9)
+font3 = ("Helvetica", 11)
+font4 = ("Comic", 8)
+
+top_tabs = sg.TabGroup(
+    [[sg.Tab("Testing1", [[sg.T(text, font=font1)]], font=font1),
+      sg.Tab("Testing2", [[sg.T(text, font=font2)]], font=font2)]],
+    font=font1, title_color="blue", selected_title_color="red", background_color="green"
+)
+bottom_tabs = sg.TabGroup(
+    [[sg.Tab("Testing3", [[sg.T(text, font=font3)]], font=font3),
+      sg.Tab("Testing4", [[sg.T(text, font=font4)]], font=font4)]],
+    font=font2
 )
 
-mult_size = (num * 3 - 1, num)
-font_sizes = [10, 15, 20]
-layout = [
-    [sg.Multiline(text, size=mult_size, font=("Courier New", i), key=str(i))]
-    for i in font_sizes
-]
-layout.append([sg.B('test')])
-window = sg.Window("test", layout, margins=(0, 0), finalize=True)
+layout = [[top_tabs], [bottom_tabs]]
+window = sg.Window("test", layout, finalize=True)
 
 while True:
-    event, values = window.read()
+    event, values = window.Read()
     if event is None or event == 'Exit':
         break
-    for i in font_sizes:
-        window[str(i)].Widget.config(width=8)
-
-window.close()
+window.Close()
