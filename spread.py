46722754ce57628d4d8f6df40f6b91f55ebc0669
JaydenKing32 <jayden.king@mq.edu.au>
2020-03-07 20:22:22 +1100

    Improved output

--- spread.py
+++ spread.py
@@ -3,29 +3,42 @@ import os
 
 import re
 
-re_algo = re.compile(r">+ (.*) <+")
-re_cost = re.compile(r".* total cost: \$(\d+\.?\d*)")
-re_turn = re.compile(r".* avg turnaround time: (\d+)")
 
-folder = "results"
+def make_spreadsheet():
+    folder = "results"
+    re_turn = re.compile(r".* avg turnaround time: (\d+)")
 
-with open("out.csv", 'w', newline='') as csv_f:
-    writer = csv.writer(csv_f)
+    with open("out.csv", 'w', newline='') as csv_f:
+        writer = csv.writer(csv_f)
+        writer.writerow(["failure trace", "ff", "bf", "minwj", "csa"])
+        servers = -1
+        config = ""
 
-    for filename in os.listdir(folder):
-        with open("{}/{}".format(folder, filename), 'r') as log:
-            title = filename.split('-')
-            conf = title[0]
-            load = "{}-{}".format(title[1], title[2])
-            model = title[3]
+        for filename in os.listdir(folder):
+            with open("{}/{}".format(folder, filename), 'r') as log:
+                title = filename.split('-')
+                length = title[1]
+                load = title[2]
+                trace = title[3]
 
-            writer.writerow([conf, load, model])
+                new_servers = title[0].split('g')[1]
+                if new_servers != servers:
+                    writer.writerow([new_servers])
+                    servers = new_servers
 
-            for line in log:
-                if line[0] == '>':
-                    algo = re_algo.match(line).group(1)
-                elif line.startswith("# total"):
-                    cost = re_cost.match(line).group(1)
-                elif line.startswith("# avg"):
-                    turn = re_turn.match(line).group(1)
-                    writer.writerow([algo, cost, turn])
+                new_config = "{}-{}".format(length, load)
+                if new_config != config:
+                    writer.writerow([new_config])
+                    config = new_config
+
+                times = []
+
+                for line in log:
+                    if line.startswith("# avg"):
+                        turn = re_turn.match(line).group(1)
+                        times.append(turn)
+
+                writer.writerow([trace] + times)
+
+
+make_spreadsheet()
